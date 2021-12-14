97891fc4c175b835ab6b872a9b78684b0e1c0cb9
Marco Yeung <myeung@redhat.com>
2021-12-14 09:58:41 -0500

    project scaffolding

---
+++ api/v1alpha1/crdbcluster_types.go
@@ -0,0 +1,64 @@
+/*
+Copyright 2021.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// CrdbClusterSpec defines the desired state of CrdbCluster
+type CrdbClusterSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// ClusterName is name of the cluster a user is creating
+	ClusterName string `json:"clusterName,omitempty"`
+}
+
+// CrdbClusterStatus defines the observed state of CrdbCluster
+type CrdbClusterStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// CrdbCluster is the Schema for the crdbclusters API
+type CrdbCluster struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   CrdbClusterSpec   `json:"spec,omitempty"`
+	Status CrdbClusterStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// CrdbClusterList contains a list of CrdbCluster
+type CrdbClusterList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []CrdbCluster `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&CrdbCluster{}, &CrdbClusterList{})
+}
